57143e4699f1e1c4fe5a2473fd5e2c7b53471263
Rogelio Aguilar <raguilar27@github.com>
2021-08-13 12:02:51 -0700

    created func to check if board is solved

--- solver.py
+++ solver.py
@@ -11,23 +11,45 @@ board = [
 ]
 
 
+def solve(board):
+    find =  find_empty(board)
+    # finds solution base case
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        # insert value
+        if valid(board, i, (row, col)):
+            board[row][col] = i
+            # if you find solution
+            if solve(board):
+                return True
+            # can't find solution 
+            # backtrack and reset prev value
+            board[row][col] = 0
+
+    return False
+
+
 def valid(board, num, pos):
-    #Check row
+    # Check row
     for i in range(len(board[0])):
-        if board[pos[0][i]] == num and pos[1] != i:
+        if board[pos[0]][i] == num and pos[1] != i:
             return False
 
-    #Check column
-    for i range(len(board)):
-        if board[pos[i][1]] == num and pos[0] != i:
+    # Check column
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
             return False
 
-    #Check box
+    # Check box
     box_x = pos[1] // 3
     box_y = pos[0] // 3
 
-    for i in range(len(box_y * 3, box_y * 3 + 3)):
-        for j in range(len(box_x * 3, box_x * 3 + 3)):
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
             if board[i][j] == num and (i, j) != pos:
                 return False
 
@@ -53,4 +75,17 @@ def find_empty(board):
     for i in range(len(board)):
         for j in range(len(board)):
             if board[i][j] == 0:
-                return (i, j)  #row, col
+                return (i, j)  # row, col
+
+    return None
+
+
+def main():
+    print_board(board)
+    solve(board)
+    print("_______________________")
+    print_board(board)
+
+
+if __name__ == "__main__":
+    main()
