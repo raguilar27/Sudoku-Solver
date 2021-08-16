9ac1473a425fc752b29a555d71f24e921f996d08
Rogelio Aguilar <raguilar27@github.com>
2021-08-15 21:48:27 -0700

    fixed linting problems

--- GUI.py
+++ GUI.py
@@ -1,8 +1,5 @@
 import pygame
-import os
-from solver import valid, find_empty
 import time
-os.environ["SDL_VIDEODRIVER"] = "dummy"
 
 pygame.font.init()
 
@@ -60,8 +57,8 @@ class Grid:
                 thick = 4
             else:
                 thick = 1
-        pygame.draw.line(self.win, (0, 0, 0), (0, i*gap), (self.width, i*gap), thick)
-        pygame.draw.line(self.win, (0, 0, 0), (i*gap, 0), (i*gap, self.height), thick)
+            pygame.draw.line(self.win, (0, 0, 0), (0, i*gap), (self.width, i*gap), thick)
+            pygame.draw.line(self.win, (0, 0, 0), (i*gap, 0), (i*gap, self.height), thick)
 
         # Draw cubes
         for i in range(self.rows):
@@ -73,6 +70,7 @@ class Grid:
         for i in range(self.rows):
             for j in range(self.cols):
                 self.cubes[i][j].selected = False
+
         self.cubes[row][col].selected = True
         self.selected = (row, col)
 
@@ -201,6 +199,38 @@ class Cube:
         self.temp = val
 
 
+def find_empty(border):
+    for i in range(len(border)):
+        for j in range(len(border[0])):
+            if border[i][j] == 0:
+                return (i, j)  # row, col
+
+    return None
+
+
+def valid(border, num, pos):
+    # Check row
+    for i in range(len(border[0])):
+        if border[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(border)):
+        if border[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if border[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+
 def redraw_window(win, board, time, strikes):
     win.fill((255, 255, 255))
     # Draw Time
@@ -278,6 +308,7 @@ def main():
                 if event.key == pygame.K_DELETE:
                     board.clear()
                     key = None
+
                 if event.key == pygame.K_SPACE:
                     board.solve_gui()
 
@@ -301,7 +332,7 @@ def main():
                     board.select(clicked[0], clicked[1])
                     key = None
 
-        if board.selected and key is not None:
+        if board.selected and key != None:
             board.sketch(key)
 
         redraw_window(win, board, play_time, strikes)
